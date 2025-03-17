a8eb9b7b7850e7e8a0a008fe49dd1e537e7dc986
spinnelattelin <eric.mitchell10@gmail.com>
2025-03-17 15:54:33 -0400

    update with the first part of the player.py file

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    background = (0,0,0)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(background)
+        
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "main":
     main()
-
 main()
+
