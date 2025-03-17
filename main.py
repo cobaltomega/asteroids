4d95730ef3f0604d2f134ba785273122e520290a
spinnelattelin <eric.mitchell10@gmail.com>
2025-03-17 13:55:57 -0400

    adding pygame screen fill effect

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pygame
+from constants import *
 
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    background = (0,0,0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(background)
+        pygame.display.flip()
 
 if __name__ == "main":
     main()
+
+main()
