27edd841250f7b8581be2e9292d01717e390f820
root <root@DESKTOP-5DG12KM.>
2025-03-20 19:49:26 -0400

    added movement

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -16,10 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+
         player.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
